db44a16a0ba73682bdfeb25a9abaf79fcacc3d12
Matt Withoos <mattwithoos@gmail.com>
2019-02-18 10:57:34 +1100

    creating token approval and execute

--- IPToken.sol
+++ IPToken.sol
@@ -37,3 +37,8 @@ contract ERC20Interface {
     event Transfer(address indexed from, address indexed to, uint tokens);
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
+
+
+contract ApproveAndCallFallBack {
+    function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
+}
